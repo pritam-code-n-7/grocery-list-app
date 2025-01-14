28c4503b9f2fd749c0d02f94dc7c1d4ae4d45a26
pritam-code-n-7 <pritamimca1003@gmail.com>
2025-01-14 11:57:15 +0530

    Added vercel.json file

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,9 @@ import express from "express";
 import cors from "cors";
 import helmet from "helmet";
 
-import dbConnect from "../config/db.conn.js";
-import groceriesRoute from "../routes/grocery.route.js";
-import { errorMiddleware } from "../middlewares/error.middleware.js";
+import dbConnect from "./config/db.conn.js";
+import groceriesRoute from "./routes/grocery.route.js";
+import { errorMiddleware } from "./middlewares/error.middleware.js";
 
 // express instance
 const app = express();
@@ -27,12 +27,12 @@ app.use(errorMiddleware);
 
 
 // server running on this port
-// const PORT = process.env.PORT || 5000;
-// const HOST = process.env.HOST
+const PORT = process.env.PORT || 5000;
+const HOST = process.env.HOST
 
-// app.listen(PORT,HOST, (error) => {
-//   if (error) {
-//     throw error;
-//   }
-//   console.log(`server running on http://${HOST}:${PORT}`);
-// });
+app.listen(PORT,HOST, (error) => {
+  if (error) {
+    throw error;
+  }
+  console.log(`server running on http://${HOST}:${PORT}`);
+});
