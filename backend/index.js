20b98a03f444d36d5d192292ee37bf7ba4b58233
pritam-code-n-7 <pritamimca1003@gmail.com>
2025-01-14 12:06:51 +0530

    modified all env

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const app = express();
 dbConnect();
 
 // middlewares
-app.use(cors())
+app.use(cors({credentials:true,origin:process.env.FRONTEND_URL}))
 app.use(helmet())
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
