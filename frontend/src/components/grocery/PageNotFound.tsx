2a26dc2301d078bd3347cccc45ada2319247df43
pritam-code-n-7 <pritamimca1003@gmail.com>
2025-01-14 16:29:34 +0530

    make not found page responsive

--- frontend/src/components/grocery/PageNotFound.tsx
+++ frontend/src/components/grocery/PageNotFound.tsx
@@ -4,8 +4,8 @@ import { Link } from "react-router-dom"
 export default function PageNotFound() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-background">
-      <h1 className="text-4xl font-bold mb-4">404 - Page Not Found</h1>
-      <p className="text-xl mb-8 text-muted-foreground">Oops! The page you're looking for doesn't exist.</p>
+      <h1 className="lg:text-4xl  font-bold mb-4">404 - Page Not Found</h1>
+      <p className="lg:text-xl mb-8 text-muted-foreground">Oops! The page you're looking for doesn't exist.</p>
       <Button asChild>
         <Link to="/">
           Go back home
