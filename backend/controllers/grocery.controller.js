af0456538ea90890710bff39f5960efecdfa294a
pritam-code-n-7 <pritamimca1003@gmail.com>
2025-01-22 11:04:31 +0530

    fixed

--- backend/controllers/grocery.controller.js
+++ backend/controllers/grocery.controller.js
@@ -44,7 +44,7 @@ export const patchItem = async (req, res) => {
   console.log(data);
   return res.status(200).json({
     success: true,
-    message: item.status===true ? "Item checked successfully" : "Item unchecked sucessfully",
+    message: item.status ? "Item checked successfully" : "Item unchecked sucessfully",
     data
   });
 };
